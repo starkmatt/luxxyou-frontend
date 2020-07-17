c5c09645520b6496c1f71db63bf10ace0c0b5904
starkmatt <mattgar92@hotmail.com>
2020-07-17 13:47:35 +1000

    Terraform files

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "project_name" {}
+variable "region"{}
+variable "azs" {}
+variable "cidr_block_all"{}
+variable "vpc_cidr"{}
+variable "subnet_private_name"{}
+variable "subnet_private_cidr"{}
+variable "subnet_public_name"{}
+variable "subnet_public_cidr"{}
+variable "rule_no_acl" {}
+variable "iam_policy_arn_task" {}
+variable "image_id" {}
+variable "instance_type" {}
+variable "iam_policy_arn_ec2" {}
